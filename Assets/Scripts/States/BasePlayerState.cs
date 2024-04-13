9ca5630d91bbd7b1f3706ea6411a2c300ab33ece
Samantha Difeo <sdifeo1@gmail.com>
2024-04-12 23:52:51 -0400

    Base state machine system

---
+++ Assets/Scripts/States/BasePlayerState.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePlayerState : MonoBehaviour
+{
+    public virtual void Enter()
+    {
+    }
+    
+    public virtual void Tick()
+    {
+        
+    }
+    
+    public virtual void Exit()
+    {
+    }
+}
