d89c2625125e46550aac2371ecfa8fd213f1d6f7
Ness Chu <vanessa.chu@mail.mcgill.ca>
2024-04-13 04:45:40 -0400

    Check if target even exists

--- Assets/Scripts/ObjectiveArrow.cs
+++ Assets/Scripts/ObjectiveArrow.cs
@@ -15,23 +15,26 @@ public class ObjectiveArrow : MonoBehaviour
     }
 
     void Update () {
-        Vector3 targetPos = cam.WorldToScreenPoint(GlobalManager.Instance.Target.gameObject.transform.position);
-        if (!Screen.safeArea.Contains(targetPos)) {
-            GetComponent<Image>().enabled = true;
-            Vector3 screenMiddle = new Vector3(Screen.width/2, Screen.height/2, 0); 
-            float angle = 360 - (Mathf.Atan2(targetPos.x-screenMiddle.x, targetPos.y-screenMiddle.y) * 180 / Mathf.PI);
-            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, angle);
+        if (GlobalManager.Instance.Target != null) {
+            Vector3 targetPos = cam.WorldToScreenPoint(GlobalManager.Instance.Target.gameObject.transform.position);
+            if (!Screen.safeArea.Contains(targetPos)) {
+                GetComponent<Image>().enabled = true;
+                Vector3 screenMiddle = new Vector3(Screen.width/2, Screen.height/2, 0); 
+                float angle = 360 - (Mathf.Atan2(targetPos.x-screenMiddle.x, targetPos.y-screenMiddle.y) * 180 / Mathf.PI);
+                transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, angle);
 
-            var height = targetPos.y - screenMiddle.y;
-            var rectTransform = GetComponent<RectTransform>();
-            rectTransform.position = Camera.main.WorldToScreenPoint(GlobalManager.Instance.Target.gameObject.transform.position);
-            if (rectTransform.position.x > Screen.width - buffer) rectTransform.position = new Vector2(Screen.width - buffer, rectTransform.position.y);
-            if (rectTransform.position.x < buffer) rectTransform.position = new Vector2(buffer, rectTransform.position.y);
-            if (rectTransform.position.y > Screen.height - buffer) rectTransform.position = new Vector2(rectTransform.position.x, Screen.height - buffer);
-            if (rectTransform.position.y < buffer) rectTransform.position = new Vector2(rectTransform.position.x, buffer);
+                var height = targetPos.y - screenMiddle.y;
+                var rectTransform = GetComponent<RectTransform>();
+                rectTransform.position = Camera.main.WorldToScreenPoint(GlobalManager.Instance.Target.gameObject.transform.position);
+                if (rectTransform.position.x > Screen.width - buffer) rectTransform.position = new Vector2(Screen.width - buffer, rectTransform.position.y);
+                if (rectTransform.position.x < buffer) rectTransform.position = new Vector2(buffer, rectTransform.position.y);
+                if (rectTransform.position.y > Screen.height - buffer) rectTransform.position = new Vector2(rectTransform.position.x, Screen.height - buffer);
+                if (rectTransform.position.y < buffer) rectTransform.position = new Vector2(rectTransform.position.x, buffer);
+            }
+            else {
+                GetComponent<Image>().enabled = false;
+            }
         }
-        else {
-            GetComponent<Image>().enabled = false;
-        }
+        else GetComponent<Image>().enabled = false;
     }
 }
