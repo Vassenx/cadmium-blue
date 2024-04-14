40c785e8ca94392819efbb9a974f242422310157
BrokenSm4sh <charles.sterlin@hotmail.com>
2024-04-14 14:27:04 -0400

    meal status data

--- Assets/Scripts/UI/MealContainer.cs
+++ Assets/Scripts/UI/MealContainer.cs
@@ -13,8 +13,25 @@ public class MealContainer:MonoBehaviour
         mealImage.gameObject.SetActive(false);
     }
 
-    public void InitMeal(Meal currentMeal)
+    public void InitMeal(Meal currentMeal, int quality)
     {
         mealImage.gameObject.SetActive(true);
+        switch (quality)
+        {
+            case 0:
+                mealImage.sprite = currentMeal.OvercookedImage;
+                break;
+            case 1:
+                mealImage.sprite = currentMeal.RawImage;
+                break;
+            case 2:
+                mealImage.sprite = currentMeal.GoodImage;
+                break;
+            case 3:
+                mealImage.sprite = currentMeal.GreatImage;
+                break;
+            default:
+                break;
+        }
     }
 }
